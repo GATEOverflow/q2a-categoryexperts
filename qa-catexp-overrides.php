0a03cb011e7f43c208157ce3afc6c85c99c236d0
Arjun <arjun@armi.in>
2017-11-03 02:43:26 +0530

    query fix

--- qa-catexp-overrides.php
+++ qa-catexp-overrides.php
@@ -12,13 +12,13 @@ function qa_db_points_update_ifuser($userid, $columns)
 select categoryid from ^categories where parentid = b.categoryid)
 or userid_src.categoryid  in  (
 select categoryid from ^categories where parentid in (select categoryid from ^categories where parentid = b.categoryid)))";
-	$query = "replace into ^catpoints (categoryid, userid, points, netvotes, aselects)  select  b.categoryid,$, ".
+	$query = "insert into ^catpoints (categoryid, userid, points, netvotes, aselects)  (select  b.categoryid,$, ".
 $calculations['aselecteds']['multiple']."*(select  ".$calculations['aselecteds']['formula'].$catfilter.")+".
 $calculations['avoteds']['multiple']."*(select  ".$calculations['avoteds']['formula'].$catfilter.")
 as points,
 (select  ".$calculations['avoteds']['formula'].$catfilter.") as netvotes,
 (select  ".$calculations['aselecteds']['formula'].$catfilter.") as aselects
- from  ^categories b";
+ from  ^categories b) on duplicate key update ^catpoints.points=points, ^catpoints.netvotes=netvotes, ^catpoints.aselects=aselects";
 
 	qa_db_query_raw(str_replace('~', "='".qa_db_escape_string($userid)."'", qa_db_apply_sub($query, array($userid))));
 								// build like this so that a #, $ or ^ character in the $userid (if external integration) isn't substituted
