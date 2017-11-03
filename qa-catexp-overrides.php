a75cda7443c41c74c849cacc6ef33e6e11b0c278
Arjun <arjun@armi.in>
2017-11-03 14:48:07 +0530

    query fix 1

--- qa-catexp-overrides.php
+++ qa-catexp-overrides.php
@@ -8,22 +8,25 @@ function qa_db_points_update_ifuser($userid, $columns)
 	if(qa_opt('qa_catexp_enable') == 1)
 	{
 		$calculations=qa_db_points_calculations();
-	$catfilter = " and (userid_src.categoryid = b.categoryid or userid_src.categoryid  in  (
-select categoryid from ^categories where parentid = b.categoryid)
-or userid_src.categoryid  in  (
-select categoryid from ^categories where parentid in (select categoryid from ^categories where parentid = b.categoryid)))";
-	$query = "insert into ^catpoints (categoryid, userid, points, netvotes, aselects)  (select  b.categoryid,$, ".
-$calculations['aselecteds']['multiple']."*(select  ".$calculations['aselecteds']['formula'].$catfilter.")+".
-$calculations['avoteds']['multiple']."*(select  ".$calculations['avoteds']['formula'].$catfilter.")
-as points,
-(select  ".$calculations['avoteds']['formula'].$catfilter.") as netvotes,
-(select  ".$calculations['aselecteds']['formula'].$catfilter.") as aselects
- from  ^categories b) on duplicate key update ^catpoints.points=points, ^catpoints.netvotes=netvotes, ^catpoints.aselects=aselects";
+		//$catfilter = "";
+//		$catfilter = "  and (userid_src.categoryid = b.categoryid)";// or userid_src.categoryid  in  (
+		$catfilter = "  and (userid_src.catidpath1 =b.categoryid or userid_src.catidpath2 = b.categoryid or userid_src.categoryid = b.categoryid)";// b.categoryid)";// or userid_src.categoryid  in  (
+		$query1 = "insert into ^catpoints (categoryid, userid, points)  (select  b.categoryid,#, ".
+			$calculations['aselecteds']['multiple']."*(select  ".$calculations['aselecteds']['formula'].$catfilter.")+".
+			$calculations['avoteds']['multiple']."*(select  ".$calculations['avoteds']['formula'].$catfilter.") 
+			as points from  ^categories b) on duplicate key update ^catpoints.points=points";
+		$query2 = "insert into ^catpoints (categoryid, userid, netvotes)  (select  b.categoryid,$, ".
+			"(select  ".$calculations['avoteds']['formula'].$catfilter.") as netvotes
+			from  ^categories b) on duplicate key update ^catpoints.netvotes=netvotes";
+		$query3 = "insert into ^catpoints (categoryid, userid, aselects)  (select  b.categoryid,$, ".
+			"(select  ".$calculations['aselecteds']['formula'].$catfilter.") as aselects
+			from  ^categories b) on duplicate key update ^catpoints.aselects=aselects";
 
-	qa_db_query_raw(str_replace('~', "='".qa_db_escape_string($userid)."'", qa_db_apply_sub($query, array($userid))));
-								// build like this so that a #, $ or ^ character in the $userid (if external integration) isn't substituted
+		qa_db_query_raw(str_replace('~', "='".qa_db_escape_string($userid)."'", qa_db_apply_sub($query1, array($userid))));
+	//	qa_db_query_raw(str_replace('~', "='".qa_db_escape_string($userid)."'", qa_db_apply_sub($query2, array($userid))));
+	//	qa_db_query_raw(str_replace('~', "='".qa_db_escape_string($userid)."'", qa_db_apply_sub($query3, array($userid))));
 	}
 }
 
-								
+
 ?>
